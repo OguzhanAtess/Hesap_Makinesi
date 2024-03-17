a95e657ca6d0131aaa19411e955f3ab9bb0766f9
OguzhanAtess <oguzhn.atess@gmail.com>
2024-03-17 15:40:09 +0300

    Hesap Makinesi

---
+++ app/src/main/java/com/example/hesap_makinesi/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.hesap_makinesi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    double ilkSayi;
+    String operation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button num0 = findViewById(R.id.num0);
+        Button num1 = findViewById(R.id.num1);
+        Button num2 = findViewById(R.id.num2);
+        Button num3 = findViewById(R.id.num3);
+        Button num4 = findViewById(R.id.num4);
+        Button num5=  findViewById(R.id.num5);
+        Button num6 = findViewById(R.id.num6);
+        Button num7 = findViewById(R.id.num7);
+        Button num8 = findViewById(R.id.num8);
+        Button num9 = findViewById(R.id.num9);
+
+        Button on = findViewById(R.id.on);
+        Button off = findViewById(R.id.off);
+        Button ac = findViewById(R.id.ac);
+        Button bolum = findViewById(R.id.bolum);
+        Button carpım = findViewById(R.id.carpım);
+        Button cıkartma = findViewById(R.id.cıkartma);
+        Button toplam = findViewById(R.id.toplam);
+        Button esittir = findViewById(R.id.esittir);
+        Button vırgul = findViewById(R.id.virgul);
+        Button del = findViewById(R.id.del);
+
+        TextView screen = findViewById(R.id.screen);
+
+        ac.setOnClickListener(view -> {
+            ilkSayi = 0;
+            screen.setText("0");
+        });
+
+        off.setOnClickListener(view -> screen.setVisibility(View.GONE));
+        on.setOnClickListener(view -> {
+            screen.setVisibility(View.VISIBLE);
+            screen.setText("0");
+        });
+
+        ArrayList<Button> nums =  new ArrayList<>();
+        nums.add(num0);
+        nums.add(num1);
+        nums.add(num2);
+        nums.add(num3);
+        nums.add(num4);
+        nums.add(num5);
+        nums.add(num6);
+        nums.add(num7);
+        nums.add(num8);
+        nums.add(num9);
+
+        for (Button b : nums) {
+            b.setOnClickListener(view -> {
+                if (!screen.getText().toString().equals("0")) {
+                    screen.setText(screen.getText().toString() + b.getText().toString());
+                } else {
+                    screen.setText(b.getText().toString());
+                }
+            });
+        }
+
+        ArrayList<Button> opers = new ArrayList<>();
+        opers.add(bolum);
+        opers.add(carpım);
+        opers.add(toplam);
+        opers.add(cıkartma);
+        for(Button b: opers) {
+            b.setOnClickListener(view -> {
+                ilkSayi = Double.parseDouble(screen.getText().toString());
+                operation = b.getText().toString();
+                screen.setText("0");
+
+            });
+        }
+
+        del.setOnClickListener(view -> {
+            String num = screen.getText().toString();
+            if (num.length()>1) {
+                screen.setText(num.substring(0, num.length()-1));
+            } else if (num.length() == 1 && !num.equals("0")) {
+                screen.setText("0");
+
+            }
+        });
+
+        vırgul.setOnClickListener(view -> {
+            if (screen.getText().toString().contains(",")) {
+                screen.setText(screen.getText().toString() + ",");
+            }
+        });
+        esittir.setOnClickListener(view -> {
+            double ikinciSayi = Double.parseDouble(screen.getText().toString());
+            double result;
+            switch (operation) {
+                case "/":
+                    result = ilkSayi/ikinciSayi;
+                    break;
+                case "X":
+                    result = ilkSayi * ikinciSayi;
+                    break;
+                case "+":
+                    result = ilkSayi + ikinciSayi;
+                    break;
+                case "-":
+                    result = ilkSayi - ikinciSayi;
+                    break;
+                default:
+                    result = ilkSayi+ikinciSayi;
+
+
+            }
+            screen.setText(String.valueOf(result));
+            ilkSayi = result;
+
+        });
+    }
+}
